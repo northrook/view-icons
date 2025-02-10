96203a817c65a0d2497ec5dc0199ce3b129687a6
Martin <hello@northrook.com>
2025-02-10 17:04:09 +0000

    mend

--- src/IconElement.php
+++ src/IconElement.php
@@ -11,7 +11,7 @@ use Core\View\Element\Attributes;
  *
  * @author Martin Nielsen <mn@northrook.com>
  */
-final class IconElement extends ViewElement
+final class IconElement extends Element
 {
     public readonly bool $isValid;
 
